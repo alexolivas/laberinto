37c7c40e3e8a8d328b2b590a12db61eb696dcd75
aolivas <alex.olivas@bluvector.io>
2018-02-21 17:55:17 -0500

    Command line application main class

---
+++ src/main/java/laberinto/CommandLineApplication.java
@@ -0,0 +1,9 @@
+package laberinto;
+
+public class CommandLineApplication {
+
+    public static void main(String[] args) {
+        // Here I will run the map generator and other features on the command line
+        System.out.println("Inside the command line application");
+    }
+}
