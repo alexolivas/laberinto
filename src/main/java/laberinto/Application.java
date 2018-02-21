16f8f151852b26e177b9e50445cd9f0e172653ab
aolivas <alex.olivas@bluvector.io>
2018-02-21 17:38:11 -0500

    initial commit

---
+++ src/main/java/laberinto/Application.java
@@ -0,0 +1,11 @@
+package laberinto
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
