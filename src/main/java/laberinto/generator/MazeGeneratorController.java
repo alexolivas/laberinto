bc1e9ad1709e60863ba51fde76faba737c81eceb
aolivas <alex.olivas@bluvector.io>
2018-02-21 18:34:19 -0500

    initial testing for maze generator controller

--- src/main/java/laberinto/generator/MazeGeneratorController.java
+++ src/main/java/laberinto/generator/MazeGeneratorController.java
@@ -1,4 +1,16 @@
 package laberinto.generator;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MazeGeneratorController {
+
+    @RequestMapping("/generate")
+    public Maze generate() {
+        System.out.println("generating a new maze");
+        return new Maze();
+    }
+
 }
