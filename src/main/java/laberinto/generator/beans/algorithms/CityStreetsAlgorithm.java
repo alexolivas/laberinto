2b5ab0e0e7adffd73fe63155f2c0a0fca7e767d2
aolivas <alex.olivas@bluvector.io>
2018-03-02 20:49:29 -0500

    Initial commit for Ellers algorithm + the city streets algorithm I'm going to come up with to generate a random city street map

--- src/main/java/laberinto/generator/beans/algorithms/CityStreetsAlgorithm.java
+++ src/main/java/laberinto/generator/beans/algorithms/CityStreetsAlgorithm.java
@@ -2,7 +2,8 @@ package laberinto.generator.beans.algorithms;
 
 public class CityStreetsAlgorithm extends MazeGeneratorAlgorithm {
     // http://www.contralogic.com/2d-pac-man-style-maze-generation/
-    public int[][] run() {
+    public int[][] run(int dimension) {
+//        generateInitialState(dimension);
         return new int[1][1];
     }
 }
--- src/main/java/laberinto/generator/beans/algorithms/EllersAlgorithm.java
+++ src/main/java/laberinto/generator/beans/algorithms/EllersAlgorithm.java
@@ -3,7 +3,10 @@ package laberinto.generator.beans.algorithms;
 public class EllersAlgorithm extends MazeGeneratorAlgorithm {
     // http://weblog.jamisbuck.org/2010/12/29/maze-generation-eller-s-algorithm
 
-    public int[][] run() {
+    public int[][] run(int dimension) {
+        // generateInitialState(dimension);
+        // 1. initialize the cells of the first row to each exist in their own set
+        // 2.
         return new int[1][1];
     }
 }
