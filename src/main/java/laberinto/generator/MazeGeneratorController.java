88451c78cac8774e209c077d6ce22762e43b5f1f
aolivas <alex.olivas@bluvector.io>
2018-03-02 20:49:50 -0500

    print statements for debugging

--- src/main/java/laberinto/generator/MazeGeneratorController.java
+++ src/main/java/laberinto/generator/MazeGeneratorController.java
@@ -5,34 +5,37 @@ import laberinto.generator.beans.EllersMaze;
 import laberinto.generator.beans.Maze;
 import laberinto.generator.beans.RecursiveBacktrackMaze;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/maze")
 public class MazeGeneratorController {
 
     @RequestMapping("/city-map/generate")
-    public Maze generateCityMap() {
+    public Maze generateCityMap(@RequestParam(value="dimension", defaultValue="20") int dimension) {
         System.out.println("generating a new city map..");
-        // Generate a new beans using a specific algorithm; determined by request
-        // parameters (or have an endpoint for each one)
+        System.out.println("Dimension: " +dimension);
         Maze maze = new CityMap();
-        maze.generate();
+        maze.generate(10);
         return maze;
     }
 
     @RequestMapping("/ellers-algorithm/generate")
-    public Maze generateMazeUsingEllersAlgorithm() {
+    public Maze generateMazeUsingEllersAlgorithm(
+            @RequestParam(value="dimension", defaultValue="20") int dimension) {
         System.out.println("generating a beans using ellers method..");
         Maze maze = new EllersMaze();
-        maze.generate();
+        maze.generate(10);
         return maze;
     }
 
     @RequestMapping("/recursive-backtracking/generate")
-    public Maze generateMazeUsingRecursiveBackTracking() {
+    public Maze generateMazeUsingRecursiveBackTracking(
+            @RequestParam(value="dimension", defaultValue="20") int dimension) {
         System.out.println("generating a beans using recursive back tracking..");
         Maze maze = new RecursiveBacktrackMaze();
-        maze.generate();
+        maze.generate(10);
         return maze;
     }
 }
